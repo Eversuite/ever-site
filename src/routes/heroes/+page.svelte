3997e55e3a7963eb6eb833becd8137f1b850f467
Dean Lozo <dean.lozo@gmail.com>
2022-12-18 20:07:28 +0100

    Started on a heroes page

---
+++ src/routes/heroes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { Avatar, Card } from "flowbite-svelte";
+</script>
+
+<div class="flex place-items-center">
+    <Card padding='sm'>
+        <div class="flex flex-col items-center pb-4">
+            <Avatar rounded size="xl" src="https://via.placeholder.com/150" />
+            <h5>Test Hero</h5>
+
+        </div>
+
+
+    </Card>
+</div>
