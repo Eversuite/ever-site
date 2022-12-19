e82b1250ababaa8c18f973012ee550720e7464ca
Dean Lozo <dean.lozo@gmail.com>
2022-12-19 13:17:55 +0000

    Load heroes data from Supabase

--- src/routes/heroes/+page.svelte
+++ src/routes/heroes/+page.svelte
@@ -1,15 +1,18 @@
-<script>
+<script lang="ts">
     import { Avatar, Card } from "flowbite-svelte";
+	import type { PageData } from "./$types";
+
+    export let data: PageData;
+    
 </script>
 
-<div class="flex place-items-center">
+<div class="flex flex-wrap justify-center gap-3">
+    {#each data.heroData as hero (hero.id)}
     <Card padding='sm'>
         <div class="flex flex-col items-center pb-4">
             <Avatar rounded size="xl" src="https://via.placeholder.com/150" />
-            <h5>Test Hero</h5>
-
+            <h5>{ hero.name }</h5>
         </div>
-
-
     </Card>
+    {/each}
 </div>
