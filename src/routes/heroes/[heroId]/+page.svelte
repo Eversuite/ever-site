5780a78311df776a847ecfebe90c1fea762f2c30
Dean Lozo <dean.lozo@gmail.com>
2022-12-22 14:33:42 +0000

    Removed card

--- src/routes/heroes/[heroId]/+page.svelte
+++ src/routes/heroes/[heroId]/+page.svelte
@@ -4,9 +4,4 @@
     export let data;
 </script>
 
-
-<div class="flex flex-wrap justify-left gap-3">
-    <HeroCard hero="{data.hero}"/>
-</div>
-
 <p>Placeholder for {data.hero?.id} detail view</p>
