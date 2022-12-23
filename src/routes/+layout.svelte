82921b7c30f7299e8c1e7d71e73413b690f5b5e4
Lukas Spillner <spillnerlu+github@gmail.com>
2022-12-23 12:59:05 +0100

    Added Filterbar for Hero-Page (#7)
    * Added filter bar for the hero page. Added HeroTypes.ts for more type-safety when working with hero data.
    
    * Merge pull request #8
    
    * added container
    
    * Added container
    
    * fixup! Resolves #6:Set container with to `w-11/12`
    
    Co-authored-by: Dean Lozo <dean.lozo@gmail.com>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,24 @@
 <script lang="ts">
-    import '../app.postcss';
-    import { supabase } from '$lib/supabaseClient'
-    import { invalidate } from '$app/navigation'
-    import { onMount } from 'svelte'
-    import  Nav  from '$lib/components/nav/+Nav.svelte'
+	import '../app.postcss';
+	import { supabase } from '$lib/supabaseClient';
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
+	import Nav from '$lib/components/nav/+Nav.svelte';
 
-    onMount(() => {
-        const {
-            data: { subscription },
-        } = supabase.auth.onAuthStateChange(() => {
-            invalidate('supabase:auth')
-        })
+	onMount(() => {
+		const {
+			data: { subscription }
+		} = supabase.auth.onAuthStateChange(() => {
+			invalidate('supabase:auth');
+		});
 
-        return () => {
-            subscription.unsubscribe()
-        }
-    })
+		return () => {
+			subscription.unsubscribe();
+		};
+	});
 </script>
 
-<Nav/>
-<slot/>
+<Nav />
+<div class="container mx-auto w-11/12">
+	<slot />
+</div>
