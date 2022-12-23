f755abde57b4486a051d8061eda636c89db7a754
Dean Lozo <dean.lozo@gmail.com>
2022-12-23 11:09:37 +0000

    Added container

--- src/routes/heroes/+page.svelte
+++ src/routes/heroes/+page.svelte
@@ -1,30 +1,28 @@
 <script lang="ts">
-    import type {PageData} from "./$types";
-    import HeroCard from "$lib/components/hero/HeroCard.svelte";
-    import {Hero} from "$lib/components/hero/HeroTypes";
-    import {Input, Label} from "flowbite-svelte";
+	import type { PageData } from './$types';
+	import HeroCard from '$lib/components/hero/HeroCard.svelte';
+	import { Hero } from '$lib/components/hero/HeroTypes';
+	import { Input, Label } from 'flowbite-svelte';
 
-    export let data: PageData;
+	export let data: PageData;
 
-    let searchTerm = ""
-    $: heroes = heroFilter(data?.heroData, searchTerm)
+	let searchTerm = '';
+	$: heroes = heroFilter(data?.heroData, searchTerm);
 
-    function heroFilter(heroes: Hero[], term: string): Hero[] {
-        return heroes.filter(hero => contains(hero.name, term) || contains(hero.role, term));
-    }
+	function heroFilter(heroes: Hero[], term: string): Hero[] {
+		return heroes.filter((hero) => contains(hero.name, term) || contains(hero.role, term));
+	}
 
-    function contains(source: string, term: string): boolean {
-        return source.toLowerCase().indexOf(term.toLowerCase()) !== -1
-    }
+	function contains(source: string, term: string): boolean {
+		return source.toLowerCase().indexOf(term.toLowerCase()) !== -1;
+	}
 </script>
 
-<Label class="flex justify-center mb-1">
-    <!-- ml-1 and mr-1 so it doesnt fill the entire screen on mobile-->
-    <Input bind:value={searchTerm} type="text" placeholder="Filter through the heroes!" class="ml-1 mr-1"/>
-</Label>
+<!-- ml-1 and mr-1 so it doesnt fill the entire screen on mobile-->
+<Input bind:value={searchTerm} type="text" placeholder="Filter through the heroes!" class="mb-3" />
 
 <div class="flex flex-wrap justify-center gap-3">
-    {#each heroes as hero (hero.id)}
-        <HeroCard {hero}/>
-    {/each}
+	{#each heroes as hero (hero.id)}
+		<HeroCard {hero} />
+	{/each}
 </div>
