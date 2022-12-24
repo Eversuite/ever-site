00592366af248de45ebf05e52a7a6305d9f8c1cf
Lukas Spillner <spillnerlu+github@gmail.com>
2022-12-24 23:05:46 +0100

    Added preview images and a way to display them.

--- src/routes/heroes/[heroId]/+page.svelte
+++ src/routes/heroes/[heroId]/+page.svelte
@@ -1,16 +1,20 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-	import { Img } from 'flowbite-svelte';
+	import { Card, Img } from 'flowbite-svelte';
 
 	export let data: PageData;
 
 	$: hero = data?.hero;
-	$: src = `/characters/preview/${hero.id}-preview.png`;
+	$: src = `/characters/preview/${hero.id}-preview-cropped.png`;
+	$: src2 = `/characters/preview/${hero.id}-preview.png`;
 </script>
 
-<div class="flex gap-3 ">
-	<Img {src} />
-	<div>
-		{hero.description}
+<div class="flex flex-wrap gap-3">
+	<Img {src} class="border-2" />
+	<Img src={src2} class="border-2" />
+	<div class="flex">
+		<Card>
+			{hero.description}
+		</Card>
 	</div>
 </div>
