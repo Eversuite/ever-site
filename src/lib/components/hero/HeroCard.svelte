2d0521db6417825ff37d5abf700ee510182f8a86
Dean Lozo <dean.lozo@gmail.com>
2022-12-25 21:39:02 +0100

    Styling fixes

--- src/lib/components/hero/HeroCard.svelte
+++ src/lib/components/hero/HeroCard.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-  import { Avatar, Card } from "flowbite-svelte";
-  import { Hero } from "$lib/components/hero/HeroTypes";
+	import { Avatar, Card } from 'flowbite-svelte';
+	import { Hero } from '$lib/components/hero/HeroTypes';
 
-  export let hero: Hero;
+	export let hero: Hero;
 </script>
 
 <Card padding="none" href="/heroes/{hero.id}">
-  <div class="flex flex-col items-center pb-4">
-    <Avatar rounded size="xl" src="/character-portraits/{hero.id}-portrait.png" />
-    <h5 class="text-base font-semibold">{hero.name}</h5>
-    <h6 class="text-xs">{hero.role}</h6>
-  </div>
+	<div class="flex flex-col items-center pb-4">
+		<Avatar rounded size="xl" src="/character-portraits/{hero.id}-portrait.png" />
+		<h5 class="text-base font-semibold">{hero.name}</h5>
+		<h6 class="text-xs">{hero.role}</h6>
+	</div>
 </Card>
