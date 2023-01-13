20d63d914ef24d3334d6500a8e91915bac1eebe4
Dean Lozo <dean.lozo@technipfmc.com>
2023-01-13 23:04:42 +0100

    Get all the data in one go

--- src/routes/heroes/[heroId]/+page.ts
+++ src/routes/heroes/[heroId]/+page.ts
@@ -10,22 +10,19 @@ export const load: PageLoad = async (event) => {
 
 	// Load the wanted character from the database.
 	// May be optimised by getting the overall hero data from the parent page or a store but will do for now.
-	// Should probably do a "promise.all" here
-	const { data: hero } = await supabaseClient
-		.from('heroes')
-		.select()
-		.eq('id', event.params.heroId)
-		.single();
+	const heroesPromise = supabaseClient.from('heroes').select().eq('id', event.params.heroId).single();
+	const abilitiesPromise = supabaseClient.from('abilities').select().eq('source', event.params.heroId);
+	const talentsPromise = supabaseClient.from('talents').select().eq('hero', event.params.heroId);
 
-	const { data: abilities } = await supabaseClient
-		.from('abilities')
-		.select()
-		.eq('source', event.params.heroId);
+	const { hero, abilities, talents } = await Promise.all([heroesPromise, abilitiesPromise, talentsPromise]).then(
+		(values) => {
+			const hero = values[0].data as Hero;
+			const abilities = values[1].data as Ability[];
+			const talents = values[2].data as Talent[];
 
-	const { data: talents } = await supabaseClient
-		.from('talents')
-		.select()
-		.eq('hero', event.params.heroId);
+			return { hero, abilities, talents };
+		}
+	);
 
 	const talentsMap = new Map<string, Talent[]>();
 	for (let talent of talents) {
@@ -33,10 +30,15 @@ export const load: PageLoad = async (event) => {
 		talentList.push(talent);
 		talentsMap.set(talent.category, talentList);
 	}
+
 	console.log(talentsMap);
 
 	if (hero === undefined || hero === null) {
 		throw error(404, `Could not find a hero for id [${event.params.heroId}]`);
 	}
-	return { hero: hero as Hero, abilities: abilities as Ability[], talentsMap: talentsMap };
+	return {
+		hero: hero as Hero,
+		abilities: abilities as Ability[],
+		talentsMap: talentsMap
+	};
 };
