5d2cb80738675c52db9ab2b6eb2fa1772603a852
Dean Lozo <dean.lozo@gmail.com>
2023-05-13 22:33:54 +0200

    Prettier Format

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,21 +1,21 @@
 /** @type {import("tailwindcss").Config} */
 module.exports = {
-  darkMode: "class",
-  content: [
-    "./src/**/*.{html,js,svelte,ts}",
-    require("path").join(require.resolve("@skeletonlabs/skeleton"), "../**/*.{html,js,svelte,ts}")
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        "evercore": ["EVERCORE"],
-        "ardela": ["ArdelaEdgeX01"]
-      }
-    }
-  },
-  plugins: [
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/typography"),
-    ...require("@skeletonlabs/skeleton/tailwind/skeleton.cjs")()
-  ]
+	darkMode: 'class',
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		require('path').join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				evercore: ['EVERCORE'],
+				ardela: ['ArdelaEdgeX01']
+			}
+		}
+	},
+	plugins: [
+		require('@tailwindcss/forms'),
+		require('@tailwindcss/typography'),
+		...require('@skeletonlabs/skeleton/tailwind/skeleton.cjs')()
+	]
 };
