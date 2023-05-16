358d803d813abc350313c1e27ebd649d09839a19
Dean Lozo <dean.lozo@technipfmc.com>
2023-05-16 21:12:24 +0200

    Styling changes:
    * Increased base font size for basic font
    * Added ability slot under icon
    * Added shard name under icon, increased shard spacing.
    * Stopped using Ardela Edge font for card header.

--- src/routes/shards/ShardIcon.svelte
+++ src/routes/shards/ShardIcon.svelte
@@ -37,6 +37,7 @@
 			alt="image for {shard.name}"
 			class="w-24 h-24 border-4 rounded-lg border-surface-300-600-token hover:!border-primary-500"
 		/>
+		<p>{shard.name}</p>
 	</div>
 	<div data-popup="{shard.id}-popup" class="card p-4">
 		<header class="card-header text-lg font-bold text-center">{shard.name}</header>
