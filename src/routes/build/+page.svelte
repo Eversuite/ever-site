ee61ae12599684201bae9156936c2a63a12f50ba
Lukas Spillner <spillnerlu+github@gmail.com>
2023-06-12 00:28:28 +0200

    Run prettier

--- src/routes/build/+page.svelte
+++ src/routes/build/+page.svelte
@@ -34,7 +34,7 @@
 
 	const abilityValidationModalComponent: ModalComponent = {
 		ref: ModalListStrings
-	}
+	};
 
 	const heroModal: ModalSettings = {
 		type: 'component',
@@ -51,8 +51,8 @@
 			title: 'Build validation issues',
 			meta: { messages: messages },
 			response: (accepted: boolean) => shareBuild(accepted)
-		}
-	};
+		};
+	}
 
 	function shardModal(index: number): ModalSettings {
 		return {
@@ -61,8 +61,8 @@
 			title: 'Select a shard',
 			meta: { items: data?.shards, path: '/shards' },
 			response: (shard: Shard) => addShard(shard, index)
-		}
-	};
+		};
+	}
 
 	const consumableModal: ModalSettings = {
 		type: 'component',
@@ -77,61 +77,97 @@
 	const maxAbilityFrom6To9 = 3;
 	const maxAbilityFrom10To12 = 4;
 	const maxAbilityFrom13To15 = 5;
-	const slotButtons = ["Q","W","E","R"]
-	
+	const slotButtons = ['Q', 'W', 'E', 'R'];
+
 	const validationMessageFormat = (message: string, slot: string, checkPoint: number) => {
-		return {message, name: `${checkPoint}-${slot}`}
-	} 
+		return { message, name: `${checkPoint}-${slot}` };
+	};
 
 	function validateAbilityChoice() {
-		const abilityCount = [0,0,0,0];
+		const abilityCount = [0, 0, 0, 0];
 		const validityMessages = [];
-		
-		for(var i = 0; i < 15; i++){
-			if(abilityLadder[i]) {
+
+		for (var i = 0; i < 15; i++) {
+			if (abilityLadder[i]) {
 				abilityCount[slotButtons.indexOf(abilityLadder[i].slot)]++;
 			}
 
-			if(i === 1) {
+			if (i === 1) {
 				abilityCount.forEach((count, index) => {
-					if(count > maxAbilityFrom1To2) {
-						validityMessages.push(validationMessageFormat(`You have more ${slotButtons[index]}s than possible at level 2 (${maxAbilityFrom1To2}).`, slotButtons[index], i))
+					if (count > maxAbilityFrom1To2) {
+						validityMessages.push(
+							validationMessageFormat(
+								`You have more ${slotButtons[index]}s than possible at level 2 (${maxAbilityFrom1To2}).`,
+								slotButtons[index],
+								i
+							)
+						);
 					}
-				})
-			} else if(i === 4) {
+				});
+			} else if (i === 4) {
 				abilityCount.forEach((count, index) => {
-					if(count > maxAbilityFrom3To5) {
-						validityMessages.push(validationMessageFormat(`You have more ${slotButtons[index]}s than possible at level 5 (${maxAbilityFrom3To5}).`, slotButtons[index], i))
+					if (count > maxAbilityFrom3To5) {
+						validityMessages.push(
+							validationMessageFormat(
+								`You have more ${slotButtons[index]}s than possible at level 5 (${maxAbilityFrom3To5}).`,
+								slotButtons[index],
+								i
+							)
+						);
 					}
-				})
+				});
 
-				if(abilityCount[3] >= 1) {
-					validityMessages.push(validationMessageFormat(`You have more Rs than possible before level 6 (1).`, "R", i))
+				if (abilityCount[3] >= 1) {
+					validityMessages.push(
+						validationMessageFormat(`You have more Rs than possible before level 6 (1).`, 'R', i)
+					);
 				}
-			} else if(i === 8) {
+			} else if (i === 8) {
 				abilityCount.forEach((count, index) => {
-					if(count > maxAbilityFrom3To5) {
-						validityMessages.push(validationMessageFormat(`You have more ${slotButtons[index]}s than possible at level 9 (${maxAbilityFrom6To9}).`, slotButtons[index], i))
+					if (count > maxAbilityFrom3To5) {
+						validityMessages.push(
+							validationMessageFormat(
+								`You have more ${slotButtons[index]}s than possible at level 9 (${maxAbilityFrom6To9}).`,
+								slotButtons[index],
+								i
+							)
+						);
 					}
-				})
-			} else if(i === 11) {
+				});
+			} else if (i === 11) {
 				abilityCount.forEach((count, index) => {
-					if(count > maxAbilityFrom10To12) {
-						validityMessages.push(validationMessageFormat(`You have more ${slotButtons[index]}s than possible at level 12 (${maxAbilityFrom10To12}).`, slotButtons[index], i))
+					if (count > maxAbilityFrom10To12) {
+						validityMessages.push(
+							validationMessageFormat(
+								`You have more ${slotButtons[index]}s than possible at level 12 (${maxAbilityFrom10To12}).`,
+								slotButtons[index],
+								i
+							)
+						);
 					}
-				})
+				});
 
-				if(abilityCount[3] >= 2) {
-					validityMessages.push(validationMessageFormat(`You have more Rs than possible before level 12 (2).`, "R", i))
+				if (abilityCount[3] >= 2) {
+					validityMessages.push(
+						validationMessageFormat(`You have more Rs than possible before level 12 (2).`, 'R', i)
+					);
 				}
-			} else if(i === 14) {
+			} else if (i === 14) {
 				abilityCount.forEach((count, index) => {
-					if(count > maxAbilityFrom10To12) {
-						validityMessages.push(validationMessageFormat(`You have more ${slotButtons[index]}s than possible at level 15 (${maxAbilityFrom13To15}).`, slotButtons[index], i))
+					if (count > maxAbilityFrom10To12) {
+						validityMessages.push(
+							validationMessageFormat(
+								`You have more ${slotButtons[index]}s than possible at level 15 (${maxAbilityFrom13To15}).`,
+								slotButtons[index],
+								i
+							)
+						);
 					}
-				})
-				if(abilityCount[3] > 3) {
-					validityMessages.push(validationMessageFormat(`You have more Rs than possible at level 15 (3).`, "R", i))
+				});
+				if (abilityCount[3] > 3) {
+					validityMessages.push(
+						validationMessageFormat(`You have more Rs than possible at level 15 (3).`, 'R', i)
+					);
 				}
 			}
 		}
@@ -166,8 +202,8 @@
 
 	async function handleBuildClick() {
 		let messages = validateAbilityChoice();
-		if(messages.length === 0) {
-			return shareBuild(true)
+		if (messages.length === 0) {
+			return shareBuild(true);
 		}
 
 		modalStore.trigger(abilityValidationModal(messages));
@@ -175,7 +211,7 @@
 
 	function addShard(shard: Shard, index: number) {
 		if (shard) {
-			selectedShards[index] = shard; 
+			selectedShards[index] = shard;
 		}
 		shardModal(index).meta.items = data?.shards.filter((item) => !selectedShards.includes(item));
 	}
@@ -204,16 +240,14 @@
 		$page.url.searchParams.set('code', encodedBuild);
 		goto(`?${$page.url.searchParams.toString()}`);
 		copyText($page.url.toString());
-		validateAbilityChoice()
+		validateAbilityChoice();
 	}
 </script>
 
 <div class="flex flex-col flex-wrap justify-center p-8">
 	<div style="margin-bottom: 5rem;">
 		<div class="b-2 text-7xl font-bold heroDescription">BUILD CREATOR</div>
-		<div style="max-width: 850px;">
-			Placeholder for future versions.
-		</div>
+		<div style="max-width: 850px;">Placeholder for future versions.</div>
 	</div>
 	<div class="h3 font-evercore mb-3">BUILD TITLE*</div>
 	<input
@@ -246,9 +280,7 @@
 				class="{borderCss} self-center p-3 w-20 h-20 flex flex-col items-center justify-center"
 				on:click={() => handleShardClick(index)}
 				on:keyup={(e) => e.key === 'Enter' && handleShardClick(index)}
-			>
-				
-			</div>
+			/>
 			{#if selectedShards.length + index !== 4}
 				<IconMathGreater class="w-10 h-10" />
 			{/if}
@@ -262,11 +294,13 @@
 					<div
 						on:click={() => handleShardClick(index)}
 						on:keyup={(e) => e.key === 'Enter' && handleShardClick(index)}
-					>	
+					>
 						{#if shard}
 							<ShardLoadoutIcon {shard} />
 						{:else}
-							<IconPlus class="w-20 h-20 border-4 rounded-lg border-surface-300-600-token hover:!border-primary-500" />
+							<IconPlus
+								class="w-20 h-20 border-4 rounded-lg border-surface-300-600-token hover:!border-primary-500"
+							/>
 						{/if}
 					</div>
 					{#if index !== 4}
@@ -302,9 +336,7 @@
 	{#if selectedHeroAbilities && selectedHeroAbilities.length > 0}
 		<div class="h1 font-evercore mt-12">ABILITIES</div>
 		<div class="flex swapToCol justify items-center mt-4">
-			<div
-				class={`flex swapToRow items-center m-4 gap-x-1 gap-1`}
-			>
+			<div class={`flex swapToRow items-center m-4 gap-x-1 gap-1`}>
 				<div class="w-12 h-12">
 					<AbilityLoadoutIcon ability={abilitySlot(selectedHeroAbilities, 'P')} />
 				</div>
@@ -331,8 +363,8 @@
 							<div
 								on:click={() => setAbility(index, slot)}
 								on:keyup={(e) => e.key === 'Enter' && setAbility(index, slot)}
-								style= "border-radius: 4px;"
-								class= "w-12 h-12 border-2 border-surface-300-600-token hover:!border-primary-500 flex flex-col items-center justify-center mr-2"
+								style="border-radius: 4px;"
+								class="w-12 h-12 border-2 border-surface-300-600-token hover:!border-primary-500 flex flex-col items-center justify-center mr-2"
 							>
 								{#if ability?.slot === slot}
 									<span style="font-weight: bold; font-size: 1.5rem;" class={`ability${slot}`}
