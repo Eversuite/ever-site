2dece64c19b9078ac790bc184d43984e02ce7f8b
Dean Lozo <dean.lozo@technipfmc.com>
2023-06-19 14:17:53 +0200

    Fixed modal style so it matches builder

--- src/routes/heroes/[heroId]/ModalImage.svelte
+++ src/routes/heroes/[heroId]/ModalImage.svelte
@@ -7,11 +7,26 @@
 </script>
 
 {#if $modalStore[0]}
-<div class="card p-4 w-modal shadow-xl space-y-4">
+<div class="modalCard p-4 w-modal space-y-4">
 	<!-- Image -->
 	<img src={$modalStore[0]?.image} alt="Big hero skin" />
     <footer class="modal-footer {parent.regionFooter}">
-        <button class="btn {parent.buttonNeutral}" on:click={parent.onClose}>Close</button>
+        <button class="btn modalButton {parent.buttonNeutral}" on:click={parent.onClose}>Close</button>
     </footer>
 </div>
 {/if}
+
+<style lang="postcss" >
+
+.modalCard {
+		background: linear-gradient(to bottom, #0e1216 0%, #0e1216 25%, #181b1f 100%),
+			linear-gradient(rgba(0, 0, 0, 1), rgba(0, 0, 0, 1));
+	}
+
+	.modalButton {
+		background-color: #1e5e6d;
+		border: 2px solid #b7d4e9;
+		border-radius: 4px;
+		color: white;
+	}
+</style>
