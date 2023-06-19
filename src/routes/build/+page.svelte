a3c3e7cf82aca7bdd69859d0c19fee926da47a72
Patryk Trela <47740822+Patrex97@users.noreply.github.com>
2023-06-19 12:53:48 +0200

    refactor abilities leveling logic (#155)
    * refactor abilities leveling logic
    
    * refactored ability level up logic again
    
    * connect AbilityLadder component data with build page
    
    ---------
    
    Co-authored-by: Patryk <patryk.trela@venturelabs.team>
    Co-authored-by: Dean Lozo <dean.lozo@gmail.com>

--- src/routes/build/+page.svelte
+++ src/routes/build/+page.svelte
@@ -2,7 +2,6 @@
 	import { modalStore, type ModalSettings, type ModalComponent } from '@skeletonlabs/skeleton';
 	import ShardsPicker from './ShardsPicker.svelte';
 	import ModalListSelect from './ModalListSelect.svelte';
-	import ModalListStrings from './ModalListStrings.svelte';
 	import type { PageData } from '../$types';
 	import type { Shard } from '$lib/class/Shard';
 	import type { Consumable } from '$lib/class/Consumable';
@@ -14,12 +13,14 @@
 	import { page } from '$app/stores';
 	import { copyText } from 'svelte-copy';
 	import ConsumableLoadoutIcon from './ConsumableLoadoutIcon.svelte';
+	import AbilityLadder from './AbilityLadder.svelte';
 
 	export let data: PageData;
 	let selectedHero: Hero = data?.selectedHero ?? undefined;
 	let selectedHeroAbilities: Ability[] = data?.selectedHeroAbilities ?? [];
 	let abilityLadder = data?.abilityLadder ?? new Array(15).fill(null);
 	let selectedShards: (Shard | null)[] = data.selectedShards ?? new Array<Shard | null>(5);
+	let selectedAbilities: string[] = [];
 	let selectedConsumable: Consumable = data?.selectedConsumable ?? undefined;
 
 	let buildTitle = data?.buildTitle ?? '';
@@ -32,10 +33,6 @@
 		ref: ModalListSelect
 	};
 
-	const abilityValidationModalComponent: ModalComponent = {
-		ref: ModalListStrings
-	};
-
 	const heroModal: ModalSettings = {
 		type: 'component',
 		component: modalComponent,
@@ -44,16 +41,6 @@
 		response: (hero: Hero) => heroSelected(hero)
 	};
 
-	function abilityValidationModal(messages): ModalSettings {
-		return {
-			type: 'component',
-			component: abilityValidationModalComponent,
-			title: 'Build validation issues',
-			meta: { messages: messages },
-			response: (accepted: boolean) => shareBuild(accepted)
-		};
-	}
-
 	const consumableModal: ModalSettings = {
 		type: 'component',
 		component: modalComponent,
@@ -61,110 +48,6 @@
 		meta: { items: data?.consumables, path: '/consumables' },
 		response: (r: Consumable) => (selectedConsumable = r)
 	};
-
-	const maxAbilityFrom1To2 = 1;
-	const maxAbilityFrom3To5 = 2;
-	const maxAbilityFrom6To9 = 3;
-	const maxAbilityFrom10To12 = 4;
-	const maxAbilityFrom13To15 = 5;
-	const slotButtons = ['Q', 'W', 'E', 'R'];
-
-	const validationMessageFormat = (message: string, slot: string, checkPoint: number) => {
-		return { message, name: `${checkPoint}-${slot}` };
-	};
-
-	function validateAbilityChoice() {
-		const abilityCount = [0, 0, 0, 0];
-		const validityMessages = [];
-
-		for (var i = 0; i < 15; i++) {
-			if (abilityLadder[i]) {
-				abilityCount[slotButtons.indexOf(abilityLadder[i].slot)]++;
-			}
-
-			if (i === 1) {
-				abilityCount.forEach((count, index) => {
-					if (count > maxAbilityFrom1To2) {
-						validityMessages.push(
-							validationMessageFormat(
-								`You have more ${slotButtons[index]}s than possible at level 2 (${maxAbilityFrom1To2}).`,
-								slotButtons[index],
-								i
-							)
-						);
-					}
-				});
-			} else if (i === 4) {
-				abilityCount.forEach((count, index) => {
-					if (count > maxAbilityFrom3To5) {
-						validityMessages.push(
-							validationMessageFormat(
-								`You have more ${slotButtons[index]}s than possible at level 5 (${maxAbilityFrom3To5}).`,
-								slotButtons[index],
-								i
-							)
-						);
-					}
-				});
-
-				if (abilityCount[3] >= 1) {
-					validityMessages.push(
-						validationMessageFormat(`You have more Rs than possible before level 6 (1).`, 'R', i)
-					);
-				}
-			} else if (i === 8) {
-				abilityCount.forEach((count, index) => {
-					if (count > maxAbilityFrom6To9) {
-						validityMessages.push(
-							validationMessageFormat(
-								`You have more ${slotButtons[index]}s than possible at level 9 (${maxAbilityFrom6To9}).`,
-								slotButtons[index],
-								i
-							)
-						);
-					}
-				});
-			} else if (i === 11) {
-				abilityCount.forEach((count, index) => {
-					if (count > maxAbilityFrom10To12) {
-						validityMessages.push(
-							validationMessageFormat(
-								`You have more ${slotButtons[index]}s than possible at level 12 (${maxAbilityFrom10To12}).`,
-								slotButtons[index],
-								i
-							)
-						);
-					}
-				});
-
-				if (abilityCount[3] > 2) {
-					console.log(abilityCount[3]);
-					validityMessages.push(
-						validationMessageFormat(`You have more Rs than possible before level 12 (2).`, 'R', i)
-					);
-				}
-			} else if (i === 14) {
-				abilityCount.forEach((count, index) => {
-					if (count > maxAbilityFrom13To15) {
-						validityMessages.push(
-							validationMessageFormat(
-								`You have more ${slotButtons[index]}s than possible at level 15 (${maxAbilityFrom13To15}).`,
-								slotButtons[index],
-								i
-							)
-						);
-					}
-				});
-				if (abilityCount[3] > 3) {
-					validityMessages.push(
-						validationMessageFormat(`You have more Rs than possible at level 15 (3).`, 'R', i)
-					);
-				}
-			}
-		}
-
-		return validityMessages;
-	}
 
 	function handleHeroClick() {
 		modalStore.trigger(heroModal);
@@ -179,59 +62,19 @@
 		selectedHeroAbilities = abilitiesResult.data as Ability[];
 	}
 
-	function setAbility(index: number, slot: string) {
-		let ability = abilitySlot(selectedHeroAbilities, slot);
-		if (ability.slot == 'R' && !canSkillUltimate(index)) {
-			return;
-		}
-		abilityLadder[index] = ability;
-	}
-
-	function canSkillUltimate(index: number): boolean {
-		let arr = abilityLadder.slice(0, index);
-		let ultimates = 0;
-
-		for (let ult of arr) {
-			if (ult?.slot === 'R') {
-				ultimates += 1;
-			}
-		}
-
-		if (index >= 14) {
-			return ultimates < 3;
-		}
-
-		if (index >= 11) {
-			return ultimates < 2;
-		}
-
-		if (index >= 5) {
-			return ultimates < 1;
-		}
-	}
-
 	function handleConsumeableClick() {
 		modalStore.trigger(consumableModal);
 	}
 
 	async function handleBuildClick() {
-		let messages = validateAbilityChoice();
-		if (messages.length === 0) {
-			return shareBuild(true);
-		}
-
-		modalStore.trigger(abilityValidationModal(messages));
+		shareBuild(true);
 	}
 
 	function shareBuild(accepted: boolean) {
 		if (!accepted) return;
 
-		let abilityLadderIds = abilityLadder.map((ability) => {
-			if (ability) {
-				return ability.id;
-			} else {
-				return null;
-			}
+		let abilityLadderIds = selectedAbilities.map((ability) => {
+			return selectedHeroAbilities.find((item) => item.slot === ability)?.id;
 		});
 
 		let selectedShardsIds = selectedShards.map((shard) => shard?.id ?? '');
@@ -249,7 +92,7 @@
 		$page.url.searchParams.set('code', encodedBuild);
 		goto(`?${$page.url.searchParams.toString()}`);
 		copyText($page.url.toString());
-		validateAbilityChoice();
+		// validateAbilityChoice();
 	}
 </script>
 
@@ -317,30 +160,7 @@
 					<AbilityLoadoutIcon ability={abilitySlot(selectedHeroAbilities, 'R')} />
 				</div>
 			</div>
-			<div class="flex swapToCol">
-				{#each abilityLadder as ability, index}
-					<div class="flex swapToRow items-center gap-1">
-						<div class="w-12 h-12 flex flex-col items-center justify-center">
-							<span style="font-weight: bold; font-size: 1.2rem;">{index + 1}</span>
-						</div>
-						{#each ['Q', 'W', 'E', 'R'] as slot, slotIndex}
-							<div
-								on:click={() => setAbility(index, slot)}
-								on:keyup={(e) => e.key === 'Enter' && setAbility(index, slot)}
-								style="border-radius: 4px;"
-								class="w-12 h-12 border-2 border-surface-300-600-token hover:!border-primary-500 flex flex-col items-center justify-center mr-2"
-							>
-								{#if ability?.slot === slot}
-									<span style="font-weight: bold; font-size: 1.5rem;" class={`ability${slot}`}
-										>{ability.slot}</span
-									>
-								{/if}
-							</div>
-						{/each}
-					</div>
-				{/each}
-			</div>
-			<div />
+			<AbilityLadder bind:selectedAbilities />
 		</div>
 	{/if}
 
@@ -353,22 +173,6 @@
 	.buildInput {
 		max-width: 380px;
 		border-radius: 14px;
-	}
-
-	.abilityQ {
-		color: #3fa6d2;
-	}
-
-	.abilityW {
-		color: #d23f51;
-	}
-
-	.abilityE {
-		color: #803fd2;
-	}
-
-	.abilityR {
-		color: #dfaf51;
 	}
 
 	.swapToRow {
